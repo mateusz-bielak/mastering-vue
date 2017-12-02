0c9f12138b776c412c96abe5fbf79a6fccfbd707
Mateusz Bielak <mt.bielak@gmail.com>
2017-12-02 14:40:40 +0100

    Added Character component logic

--- applications/starbase/src/App.vue
+++ applications/starbase/src/App.vue
@@ -1,16 +1,25 @@
 <template>
   <div id="app">
     <h3>{{ title }}</h3>
+    <div class="col-md-12">
+      <Character :id="id" v-for="(id, index) in initial_ids" :key="index" />
+    </div>
   </div>
 </template>
 
 <script>
+import Character from './components/Character';
+
 export default {
   name: 'app',
   data() {
     return {
-      title: 'Generate Your Team'
+      title: 'Generate Your Team',
+      initial_ids: [1, 13, 14],
     }
+  },
+  components: {
+    Character
   }
 }
 </script>
