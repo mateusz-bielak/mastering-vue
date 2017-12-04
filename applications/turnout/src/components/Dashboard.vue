a85f97d35bf9ea0607caf3c0556e04c09e1b08e2
Mateusz Bielak <mt.bielak@gmail.com>
2017-12-04 17:14:24 +0100

    Signing out functionality

--- applications/turnout/src/components/Dashboard.vue
+++ applications/turnout/src/components/Dashboard.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
-    Dashboard Component
-    {{ $store.state }}
+    <h3>Events Dashboard</h3>
+    <button class="btn btn-danger btn-sm signout-btn" @click="signOut">Sign Out</button>
+    {{$store.state}}
   </div>
 </template>
+
+<script>
+import { firebaseApp } from '../firebaseApp';
+
+export default {
+  methods: {
+    signOut() {
+      this.$store.dispatch('signOut');
+      firebaseApp.auth().signOut();
+    }
+  }
+}
+</script>
--- applications/turnout/src/store/mutations.js
+++ applications/turnout/src/store/mutations.js
@@ -4,4 +4,7 @@ export const mutations = {
   [types.SIGN_IN] (state, user_payload) {
     state.user = user_payload;
   },
+  [types.SIGN_OUT] (state) {
+    state.user = {};
+  },
 };
