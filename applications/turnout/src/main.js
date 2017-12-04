d786a43f4c0e7a567785ee8d9e6f9e20a3d1e59d
Mateusz Bielak <mt.bielak@gmail.com>
2017-12-04 17:50:56 +0100

    Sign Up component

--- applications/turnout/src/main.js
+++ applications/turnout/src/main.js
@@ -3,6 +3,7 @@ import App from './components/App.vue';
 import VueRouter from 'vue-router';
 import Dashboard from './components/Dashboard.vue';
 import Signin from './components/Signin.vue';
+import Signup from './components/Signup.vue';
 import { firebaseApp } from './firebaseApp';
 import store from './store';
 
@@ -13,15 +14,16 @@ const router = new VueRouter({
   routes: [
     { path: '/dashboard', component: Dashboard },
     { path: '/signin', component: Signin },
+    { path: '/signup', component: Signup },
   ]
 })
 
 firebaseApp.auth().onAuthStateChanged(user => {
   if (user) {
-    store.dispatch('signIn', user)
-    router.push('/dashboard')
+    store.dispatch('signIn', user);
+    router.push('/dashboard');
   } else {
-    router.replace('/signin')
+    router.replace('/signin');
   }
 });
 
